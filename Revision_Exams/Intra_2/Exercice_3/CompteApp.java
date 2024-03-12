fc8a2984a81647c8c264dc9f643acecb785b884f
Vianpyro <vianney@veremme.org>
2024-03-12 17:21:16 -0400

    Ajout des tests

--- Revision_Exams/Intra_2/Exercice_3/CompteApp.java
+++ Revision_Exams/Intra_2/Exercice_3/CompteApp.java
@@ -1,7 +1,33 @@
 package Revision_Exams.Intra_2.Exercice_3;
 
+import Revision_Exams.Intra_2.Exercice_3.Exceptions.*;
+
 public class CompteApp {
     public static void main(String[] args) {
+        // Création d'un compte bancaire
+        CompteBancaire compte = new CompteBancaire("123456789", 2000);
 
+        // Opérations de retrait
+        try {
+            compte.retrait(500);
+            System.out.println("Retrait effectué avec succès.");
+        } catch (SoldeInsuffisantException e) {
+            System.out.println("Échec du retrait: " + e.getMessage());
+        }
+
+        try {
+            compte.retrait(2000);
+            System.out.println("Retrait effectué avec succès.");
+        } catch (SoldeInsuffisantException e) {
+            System.out.println("Échec du retrait: " + e.getMessage());
+        }
+
+        // Opération de virement
+        try {
+            compte.virement(1500, "987654321"); // Montant plus grand que MAX_VIREMENT
+            System.out.println("Virement effectué avec succès.");
+        } catch (SoldeInsuffisantException | LimiteDepasseeException | IllegalArgumentException e) {
+            System.out.println("Échec du virement: " + e.getMessage());
+        }
     }
 }
